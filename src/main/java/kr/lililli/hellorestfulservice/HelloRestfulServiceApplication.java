698b5cf77b58f353767dc50f172b5f19232b8ead
mkp0131 <mkp0131@naver.com>
2024-11-13 17:49:02 +0900

    1

---
+++ src/main/java/kr/lililli/hellorestfulservice/HelloRestfulServiceApplication.java
@@ -0,0 +1,25 @@
+package kr.lililli.hellorestfulservice;
+
+import java.util.Locale;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class HelloRestfulServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloRestfulServiceApplication.class, args);
+  }
+
+  //  다국어 처리
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
+
+}
---
+++ src/test/java/kr/lililli/hellorestfulservice/HelloRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.lililli.hellorestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
